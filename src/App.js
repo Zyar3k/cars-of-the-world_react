b8dcaab95b763ee0b9b8c24a5cc5aa75a0adf008
zyar3k <a.zygan@gmail.com>
2021-10-10 20:02:05 +0200

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { Route, Switch } from "react-router";
+
 import "./App.css";
+
+import CarForm from "./components/CarForm";
+import CarList from "./components/CarList";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>Init</h1>
+      <Header />
+      <Switch>
+        <Route path="/" component={CarList} exact />
+        <Route path="/add" component={CarForm} />
+      </Switch>
     </div>
   );
 }
