6404b369e7cdbbe7981903ca42b91d854778637e
zyar3k <a.zygan@gmail.com>
2021-10-10 20:05:14 +0200

    Add header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,23 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="container">
+        <div className="headerItem">
+          <input type="text" placeholder="Search by brand" />
+        </div>
+        <div className="headerItem">
+          <Link to="/">
+            <h1>Cars</h1>
+          </Link>
+        </div>
+        <div className="headerItem">
+          <button>Add car</button>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
